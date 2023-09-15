3640772724524c10bd593f3d0af054ef9285a06a
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 11:47:37 -0300

    upando arquivo

--- aula.js
+++ aula.js
@@ -1,1 +1,19 @@
+// Criando alerta em JavaScript
 alert("Olá mundo!");
+
+/*
+    ***********************
+    - Adicionando JavaScript
+    - Criando novo projeto
+    ************************
+*/
+
+//Adicionando uma mensagem na tela
+document.write('Olá Mundo!');
+
+//Adicionando console.log
+console.log("Olá eu sou o lucas")
+
+//Criando uma variável
+let mensagem = "Olá Mundo!";
+console.log("Mensagem de variável: ", mensagem);
