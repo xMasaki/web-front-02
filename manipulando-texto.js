e9ddbeabec932152370e1cfadf95be205da024b8
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 11:51:55 -0300

    Adicionando arquivos de manipulação e tipos de dados

---
+++ manipulando-texto.js
@@ -0,0 +1,12 @@
+let numFloat = 123.543;
+let linguagens = ["Java", "C#", "Python"]
+//                  0       1       2
+let txt = "Estão chegando as provas";
+// toString é uma função nativa do js
+
+// console.log(typeof numFloat.toString());
+
+// console.log(typeof String(numFloat));
+
+// console.log(linguagens[1])
+console.log(txt.replace("provas", "avaliações"));
---
+++ tipos-dados.js
@@ -0,0 +1,18 @@
+// Números inteiros
+let idadeAluno = 18;
+
+// Números flutuantes
+let valorProduto = 5.35;
+
+// String
+let nomeAluno = "Pedro Henrique";
+
+// Boolean
+let casado = true;
+
+//typeof verifica o tipo da variável
+
+// console.log(typeof idadeAluno);
+// console.log(typeof valorProduto);
+// console.log(typeof nomeAluno);
+console.log(typeof casado);
